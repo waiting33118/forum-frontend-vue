8231714a7af269dce51e0627c6bcdce27b2b2b0e
Tony <waiting33118@gmail.com>
2020-08-11 19:47:41 +0800

    feat: add navtabs routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,37 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import SignIn from '../views/SignIn.vue'
+import Restaurants from '../views/Restaurants.vue'
 import NotFound from '../views/NotFound.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'root',
+    redirect: '/restaurants'
+  },
+  {
+    path: '/restaurants',
+    name: 'restaurants',
+    component: Restaurants
+  },
+  {
+    path: '/restaurants/feeds',
+    name: 'restaurantsFeeds',
+    component: () => import('../views/RestaurantsFeeds.vue')
+  },
+  {
+    path: '/restaurants/top',
+    name: 'restaurantsTop',
+    component: () => import('../views/RestaurantsTop.vue')
+  },
+  {
+    path: '/users/top',
+    name: 'usersTop',
+    component: () => import('../views/UsersTop.vue')
+  },
   {
     path: '/signin',
     name: 'SignIn',
@@ -24,6 +50,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
+  linkExactActiveClass: 'active',
   routes
 })
 
