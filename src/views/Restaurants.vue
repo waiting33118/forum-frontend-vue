890922bfad6526b4ac10a5e88368888ff303d3ac
Tony <waiting33118@gmail.com>
2020-08-12 17:21:11 +0800

    feat: add restaurantCards component in Restaurant page

--- src/views/Restaurants.vue
+++ src/views/Restaurants.vue
@@ -1,17 +1,332 @@
 <template>
   <div class="container py-5">
     <NavTabs />
-    <h1 class="mt-5">
-      首頁 - 餐廳列表
-    </h1>
+    <div class="row">
+      <RestaurantCard
+        v-for="restaurant in restaurants"
+        :key="restaurant.id"
+        :init-restaurant="restaurant"
+      />
+    </div>
+    <!-- 分頁標籤 RestaurantPagination -->
   </div>
 </template>
 
 <script>
 import NavTabs from './../components/NavTabs'
+import RestaurantCard from './../components/RestaurantCard'
+const dummyData = {
+  restaurants: [
+    {
+      id: 5,
+      name: 'Mitchel Beatty Girl',
+      tel: '171-324-8413',
+      address: '85408 Francisca Square',
+      opening_hours: '08:00',
+      description: 'Aut cumque excepturi exercitationem libero volupta',
+      image: 'https://i.imgur.com/awJFBLn.jpg',
+      viewCounts: 438,
+      createdAt: '2020-02-28T14:38:32.000Z',
+      updatedAt: '2020-07-22T11:50:21.000Z',
+      CategoryId: 3,
+      Category: {
+        id: 3,
+        name: '義大利料理',
+        createdAt: '2020-02-28T14:38:32.000Z',
+        updatedAt: '2020-02-28T14:38:32.000Z'
+      },
+      isFavorited: false,
+      isLiked: false
+    },
+    {
+      id: 6,
+      name: 'Vicenta Gutkowski Test By Nacho',
+      tel: '128-905-3953',
+      address: '371 Reuben Vista test',
+      opening_hours: '00:00',
+      description: 'labore',
+      image: 'https://loremflickr.com/320/240/restaurant,food/?random=16.659471621888123',
+      viewCounts: 172,
+      createdAt: '2020-02-28T14:38:32.000Z',
+      updatedAt: '2020-07-22T09:21:00.000Z',
+      CategoryId: 2,
+      Category: {
+        id: 2,
+        name: '日本料理',
+        createdAt: '2020-02-28T14:38:32.000Z',
+        updatedAt: '2020-02-28T14:38:32.000Z'
+      },
+      isFavorited: false,
+      isLiked: false
+    },
+    {
+      id: 7,
+      name: 'Miss Letitia Will',
+      tel: '(770) 750-6811',
+      address: '8145 Laisha Keys',
+      opening_hours: '08:00',
+      description: 'Aut earum molestiae et ducimus adipisci ut maxime.',
+      image: 'https://i.imgur.com/50clDpn.jpg',
+      viewCounts: 117,
+      createdAt: '2020-02-28T14:38:32.000Z',
+      updatedAt: '2020-07-17T16:27:47.000Z',
+      CategoryId: 5,
+      Category: {
+        id: 5,
+        name: '素食料理',
+        createdAt: '2020-02-28T14:38:32.000Z',
+        updatedAt: '2020-02-28T14:38:32.000Z'
+      },
+      isFavorited: false,
+      isLiked: false
+    },
+    {
+      id: 9,
+      name: 'Luz Schaden',
+      tel: '1-712-742-4184',
+      address: '4929 Wisozk Centers',
+      opening_hours: '08:00',
+      description: 'Culpa deserunt molestiae eligendi est ex incidunt.',
+      image: 'https://loremflickr.com/320/240/restaurant,food/?random=6.497835218728576',
+      viewCounts: 35,
+      createdAt: '2020-02-28T14:38:32.000Z',
+      updatedAt: '2020-07-22T09:20:30.000Z',
+      CategoryId: 1692,
+      Category: {
+        id: 1692,
+        name: '泰國料理',
+        createdAt: '2020-06-10T07:46:42.000Z',
+        updatedAt: '2020-06-10T08:06:22.000Z'
+      },
+      isFavorited: false,
+      isLiked: false
+    },
+    {
+      id: 10,
+      name: 'Corbin Gusikowski',
+      tel: '(506) 198-3977',
+      address: '4747 Lubowitz Brook',
+      opening_hours: '08:00',
+      description: 'Aut quia aut ea nostrum quae doloremque necessitat',
+      image: 'https://loremflickr.com/320/240/restaurant,food/?random=6.84802493817025',
+      viewCounts: 40,
+      createdAt: '2020-02-28T14:38:32.000Z',
+      updatedAt: '2020-07-15T06:43:54.000Z',
+      CategoryId: 5,
+      Category: {
+        id: 5,
+        name: '素食料理',
+        createdAt: '2020-02-28T14:38:32.000Z',
+        updatedAt: '2020-02-28T14:38:32.000Z'
+      },
+      isFavorited: false,
+      isLiked: false
+    },
+    {
+      id: 11,
+      name: 'Vergie Considine',
+      tel: '351.951.8030',
+      address: '8865 Conrad Mill',
+      opening_hours: '08:00',
+      description: 'Quia omnis natus est praesentium dolores aliquid d',
+      image: 'https://loremflickr.com/320/240/restaurant,food/?random=0.7508871784973614',
+      viewCounts: 30,
+      createdAt: '2020-02-28T14:38:32.000Z',
+      updatedAt: '2020-07-22T09:18:49.000Z',
+      CategoryId: 1702,
+      Category: {
+        id: 1702,
+        name: '越式料理',
+        createdAt: '2020-06-10T07:48:52.000Z',
+        updatedAt: '2020-06-10T08:06:36.000Z'
+      },
+      isFavorited: false,
+      isLiked: false
+    },
+    {
+      id: 12,
+      name: 'Oliver Padberg',
+      tel: '865-523-0270',
+      address: '771 Oberbrunner Hollow',
+      opening_hours: '08:00',
+      description: 'Magni qui facilis asperiores. Voluptatem cumque co',
+      image: 'https://loremflickr.com/320/240/restaurant,food/?random=22.348939427568215',
+      viewCounts: 13,
+      createdAt: '2020-02-28T14:38:32.000Z',
+      updatedAt: '2020-07-22T09:21:23.000Z',
+      CategoryId: 1,
+      Category: {
+        id: 1,
+        name: '中式料理',
+        createdAt: '2020-02-28T14:38:32.000Z',
+        updatedAt: '2020-05-17T03:45:22.000Z'
+      },
+      isFavorited: false,
+      isLiked: false
+    },
+    {
+      id: 13,
+      name: 'Mrs. Eileen Lockman',
+      tel: '(597) 404-9974',
+      address: '383 Stroman Center',
+      opening_hours: '08:00',
+      description: 'Vitae id incidunt animi qui sapiente perferendis i',
+      image: 'https://loremflickr.com/320/240/restaurant,food/?random=54.950245749491145',
+      viewCounts: 22,
+      createdAt: '2020-02-28T14:38:32.000Z',
+      updatedAt: '2020-07-12T17:04:17.000Z',
+      CategoryId: 2,
+      Category: {
+        id: 2,
+        name: '日本料理',
+        createdAt: '2020-02-28T14:38:32.000Z',
+        updatedAt: '2020-02-28T14:38:32.000Z'
+      },
+      isFavorited: false,
+      isLiked: false
+    },
+    {
+      id: 14,
+      name: 'Boyd Abbott',
+      tel: '172-684-6335',
+      address: '50119 Nader Via',
+      opening_hours: '08:00',
+      description: 'Aut aut enim sint sed. Vero qui voluptas voluptate',
+      image: 'https://loremflickr.com/320/240/restaurant,food/?random=67.78299128798855',
+      viewCounts: 7422,
+      createdAt: '2020-02-28T14:38:32.000Z',
+      updatedAt: '2020-07-09T11:16:21.000Z',
+      CategoryId: 4,
+      Category: {
+        id: 4,
+        name: '墨西哥料理',
+        createdAt: '2020-02-28T14:38:32.000Z',
+        updatedAt: '2020-02-28T14:38:32.000Z'
+      },
+      isFavorited: false,
+      isLiked: false
+    },
+    {
+      id: 16,
+      name: 'Aaliyah Lakin',
+      tel: '1-235-923-9473 x43870',
+      address: '1951 Jakubowski Villages',
+      opening_hours: '08:00',
+      description: 'quidem',
+      image: 'https://loremflickr.com/320/240/restaurant,food/?random=91.06034163072859',
+      viewCounts: 13,
+      createdAt: '2020-02-28T14:38:32.000Z',
+      updatedAt: '2020-07-15T06:42:49.000Z',
+      CategoryId: 5,
+      Category: {
+        id: 5,
+        name: '素食料理',
+        createdAt: '2020-02-28T14:38:32.000Z',
+        updatedAt: '2020-02-28T14:38:32.000Z'
+      },
+      isFavorited: false,
+      isLiked: false
+    }
+  ],
+  categories: [
+    {
+      id: 1,
+      name: '中式料理',
+      createdAt: '2020-02-28T14:38:32.000Z',
+      updatedAt: '2020-05-17T03:45:22.000Z'
+    },
+    {
+      id: 2,
+      name: '日本料理',
+      createdAt: '2020-02-28T14:38:32.000Z',
+      updatedAt: '2020-02-28T14:38:32.000Z'
+    },
+    {
+      id: 3,
+      name: '義大利料理',
+      createdAt: '2020-02-28T14:38:32.000Z',
+      updatedAt: '2020-02-28T14:38:32.000Z'
+    },
+    {
+      id: 4,
+      name: '墨西哥料理',
+      createdAt: '2020-02-28T14:38:32.000Z',
+      updatedAt: '2020-02-28T14:38:32.000Z'
+    },
+    {
+      id: 5,
+      name: '素食料理',
+      createdAt: '2020-02-28T14:38:32.000Z',
+      updatedAt: '2020-02-28T14:38:32.000Z'
+    },
+    {
+      id: 1592,
+      name: 'dark cousine',
+      createdAt: '2020-05-17T03:28:54.000Z',
+      updatedAt: '2020-05-17T03:46:06.000Z'
+    },
+    {
+      id: 1692,
+      name: '泰國料理',
+      createdAt: '2020-06-10T07:46:42.000Z',
+      updatedAt: '2020-06-10T08:06:22.000Z'
+    },
+    {
+      id: 1702,
+      name: '越式料理',
+      createdAt: '2020-06-10T07:48:52.000Z',
+      updatedAt: '2020-06-10T08:06:36.000Z'
+    },
+    {
+      id: 1852,
+      name: '6667',
+      createdAt: '2020-07-17T08:52:03.000Z',
+      updatedAt: '2020-07-17T08:56:37.000Z'
+    },
+    {
+      id: 1862,
+      name: '1234',
+      createdAt: '2020-07-17T11:12:50.000Z',
+      updatedAt: '2020-07-17T11:12:50.000Z'
+    }
+  ],
+  categoryId: '',
+  page: 1,
+  totalPage: [
+    1,
+    2,
+    3,
+    4,
+    5
+  ],
+  prev: 1,
+  next: 2
+}
 export default {
   components: {
-    NavTabs
+    NavTabs,
+    RestaurantCard
+  },
+  data () {
+    return {
+      categories: [],
+      categoryId: '',
+      currentPage: -1,
+      restaurants: [],
+      totalPage: -1
+    }
+  },
+  created () {
+    this.fetchRestaurants()
+  },
+  methods: {
+    fetchRestaurants () {
+      this.categories = dummyData.categories
+      this.categoryId = dummyData.categoryId
+      this.currentPage = dummyData.page
+      this.restaurants = dummyData.restaurants
+      this.totalPage = dummyData.totalPage.length
+    }
   }
 }
 </script>
