766bf7d695d6067425735b5a2f6949418df5c05a
Tony <waiting33118@gmail.com>
2020-08-14 10:40:18 +0800

    feat: add restaurant detail router

--- src/router/index.js
+++ src/router/index.js
@@ -28,6 +28,11 @@ const routes = [
     component: () => import('../views/RestaurantsTop.vue')
   },
   {
+    path: '/restaurants/:id',
+    name: 'restaurant',
+    component: () => import('../views/Restaurant.vue')
+  },
+  {
     path: '/users/top',
     name: 'usersTop',
     component: () => import('../views/UsersTop.vue')
