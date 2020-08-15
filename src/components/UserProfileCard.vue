e853f63a2ea424431986b70b9ef356375f2b77a5
Tony <waiting33118@gmail.com>
2020-08-15 17:45:20 +0800

    feat: create user profile card component

---
+++ src/components/UserProfileCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="card mb-3">
+    <div class="row no-gutters">
+      <div class="col-md-4">
+        <img
+          :src="userProfile.image? userProfile.image :'https://via.placeholder.com/300'"
+        >
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">
+            {{ userProfile.name }}
+          </h5>
+          <p class="card-text">
+            {{ userProfile.email }}
+          </p>
+          <ul class="list-unstyled list-inline">
+            <li><strong>{{ userProfile.commentCounts }}</strong> 已評論餐廳</li>
+            <li><strong>{{ userProfile.favoritedRestaurantCounts }}</strong> 收藏的餐廳</li>
+            <li><strong>{{ userProfile.followingCounts }}</strong> followings (追蹤者)</li>
+            <li><strong>{{ userProfile.followerCounts }}</strong> followers (追隨者)</li>
+          </ul>
+          <p>
+            <router-link to="#">
+              <button
+                type="submit"
+                class="btn btn-primary"
+              >
+                edit
+              </button>
+            </router-link>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+  }
+</style>
+
+<script>
+export default {
+  props: {
+    userProfile: {
+      type: Object,
+      required: true
+    },
+    currentUser: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
