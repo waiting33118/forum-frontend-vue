e9945b6c89f04ce74b1113dcce4403d239c9a707
Tony <waiting33118@gmail.com>
2020-08-15 17:44:01 +0800

    feat: create user comments card component

---
+++ src/components/UserCommentsCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <strong>{{ comments.length }}</strong> 已評論餐廳
+    </div>
+    <div class="card-body">
+      <router-link
+        v-for="comment in comments"
+        :key="comment.id"
+        :to="{ name:'restaurant', params:{ id:comment.RestaurantId }}"
+      >
+        <img
+          :src="comment.Restaurant.image"
+          class="avatar"
+          alt="avatar"
+        >
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .avatar {
+    margin: 2px;
+    width: 60px;
+    height: 60px;
+    object-fit: cover;
+  }
+</style>
+
+<script>
+export default {
+  props: {
+    comments: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
