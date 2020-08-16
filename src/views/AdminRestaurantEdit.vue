de67b848cae2426ee80e5823580470bac573c83f
Tony <waiting33118@gmail.com>
2020-08-16 20:26:25 +0800

    feat: create admin restaurant edit view page

---
+++ src/views/AdminRestaurantEdit.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container py-5">
+    <AdminRestaurantForm
+      :initial-restaurant="restaurant"
+      @after-form-submit="afterFormSubmit"
+    />
+  </div>
+</template>
+
+<script>
+import AdminRestaurantForm from './../components/AdminRestaurantForm'
+const dummyData = {
+  restaurant: {
+    id: 1,
+    name: 'Laurence Reynolds',
+    tel: '1-657-067-3756 x9782',
+    address: '187 Kirlin Squares',
+    opening_hours: '08:00',
+    description: 'sit est mollitia',
+    image: 'https://loremflickr.com/320/240/restaurant,food/?random=91.29816290184887',
+    viewCounts: 1,
+    createdAt: '2019-07-30T16:24:55.432Z',
+    updatedAt: '2019-07-30T17:26:43.260Z',
+    CategoryId: 3,
+    Category: {
+      id: 3,
+      name: '義大利料理',
+      createdAt: '2019-07-30T16:24:55.429Z',
+      updatedAt: '2019-07-30T16:24:55.429Z'
+    }
+  }
+}
+export default {
+  components: {
+    AdminRestaurantForm
+  },
+  data () {
+    return {
+      restaurant: {
+        id: -1,
+        name: '',
+        categoryId: '',
+        tel: '',
+        address: '',
+        description: '',
+        image: '',
+        openingHours: ''
+      }
+    }
+  },
+  created () {
+    const { id } = this.$route.params
+    this.fetchRestaurant(id)
+  },
+  methods: {
+    afterFormSubmit (formData) {
+      for (const [key, value] of formData.entries()) {
+        console.log(key, value)
+      }
+    },
+    fetchRestaurant (restaurantId) {
+      const {
+        id,
+        name, CategoryId: categoryId,
+        tel,
+        address,
+        description,
+        image,
+        opening_hours: openingHours
+      } = dummyData.restaurant
+      this.restaurant = {
+        id,
+        name,
+        categoryId,
+        tel,
+        address,
+        description,
+        image,
+        openingHours
+      }
+    }
+  }
+}
+</script>
