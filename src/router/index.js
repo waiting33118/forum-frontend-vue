35d79fa017fe5413ce8e11616570ae4e1f07667a
Tony <waiting33118@gmail.com>
2020-08-16 13:14:53 +0800

    feat: add admin restaurant router

--- src/router/index.js
+++ src/router/index.js
@@ -58,6 +58,11 @@ const routes = [
     component: () => import('./../views/AdminRestaurants.vue')
   },
   {
+    path: '/admin/restaurants/:id',
+    name: 'admin-restaurant',
+    component: () => import('./../views/AdminRestaurant.vue')
+  },
+  {
     path: '/signin',
     name: 'SignIn',
     component: SignIn
