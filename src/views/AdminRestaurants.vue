9512a195c7694d1264c534acd68bf9d1ff9d74cd
Tony <waiting33118@gmail.com>
2020-08-16 12:42:48 +0800

    feat: create admin restaurants view page

---
+++ src/views/AdminRestaurants.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container py-5">
+    <AdminNav />
+    <a
+      href="#"
+      class="btn btn-primary mb-4"
+    >
+      New Restaurant
+    </a>
+    <AdminRestaurantsTable />
+  </div>
+</template>
+
+<script>
+import AdminNav from './../components/AdminNav'
+import AdminRestaurantsTable from './../components/AdminRestaurantsTable'
+export default {
+  components: {
+    AdminNav,
+    AdminRestaurantsTable
+  }
+}
+</script>
