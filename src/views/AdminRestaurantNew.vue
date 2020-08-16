77833271083b149fe4296aeda9bb89a3dccd53c6
Tony <waiting33118@gmail.com>
2020-08-16 20:26:42 +0800

    feat: create admin restaurant new form view page

---
+++ src/views/AdminRestaurantNew.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container py-5">
+    <AdminRestaurantForm @after-form-submit="afterFormSubmit" />
+  </div>
+</template>
+
+<script>
+import AdminRestaurantForm from './../components/AdminRestaurantForm'
+export default {
+  components: {
+    AdminRestaurantForm
+  },
+  methods: {
+    afterFormSubmit (formData) {
+      for (const [key, value] of formData.entries()) {
+        console.log(key, value)
+      }
+    }
+  }
+}
+</script>
