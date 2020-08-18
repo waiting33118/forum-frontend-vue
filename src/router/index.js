1bc6785be10b2f1382d95714f11f9f2d274cfb07
Tony <waiting33118@gmail.com>
2020-08-18 12:01:47 +0800

    feat: add users edit router

--- src/router/index.js
+++ src/router/index.js
@@ -48,6 +48,11 @@ const routes = [
     component: () => import('./../views/User.vue')
   },
   {
+    path: '/users/:id/edit',
+    name: 'user-edit',
+    component: () => import('./../views/UserEdit.vue')
+  },
+  {
     path: '/admin',
     exact: true,
     redirect: '/admin/restaurants'
