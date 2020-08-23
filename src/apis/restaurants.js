fefef6d6336146eeaa50a9c14ffcd5cc294b27b4
Tony <waiting33118@gmail.com>
2020-08-23 11:29:04 +0800

    feat: create fetch method

--- src/apis/restaurants.js
+++ src/apis/restaurants.js
@@ -13,5 +13,20 @@ export default {
     return apiHelper.get('/restaurants/feeds', {
       headers: { Authorization: `Bearer ${getToken()}` }
     })
+  },
+  getTop () {
+    return apiHelper.get('/restaurants/top', {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
+  addFavorite (restaurantId) {
+    return apiHelper.post(`/favorite/${restaurantId}`, null, {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
+  deleteFavorite (restaurantId) {
+    return apiHelper.delete(`/favorite/${restaurantId}`, {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
   }
 }
