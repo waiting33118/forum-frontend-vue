b0f66efd792d79f5d8b7782c505ebe3ead605d4b
Tony <waiting33118@gmail.com>
2020-08-30 17:49:33 +0800

    feat: create interceptor for axios

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,16 @@
 import axios from 'axios'
 import Swal from 'sweetalert2'
 
-export const apiHelper = axios.create({
+const axiosInstance = axios.create({
   baseURL: 'https://forum-express-api.herokuapp.com/api'
 })
+axiosInstance.interceptors.request.use(config => {
+  const token = localStorage.getItem('token')
+  if (token) config.headers.Authorization = `Bearer ${token}`
+  return config
+})
+
+export const apiHelper = axiosInstance
 
 export const Toast = Swal.mixin({
   toast: true,
